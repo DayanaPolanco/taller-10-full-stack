1f0a8b5644714c49f6af748dcfb094f466ddad0d
VanesaPolanco <vanesa.polanco314@gmail.com>
2023-07-22 16:23:34 -0500

    Primer commit

---
+++ punto-1/punto-1.php
@@ -0,0 +1,21 @@
+<?php
+// CONSTANTS
+const PI = 3.14159;
+define("WELCOME_MESSAGE", "Bienvenida");
+
+//VARIABLES
+$firstName = "Dayana";
+$lastName = "Polanco";
+
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+
+$firstName = "Vanesa";
+$lastName = "Sierra";
+
+echo "-----------------\n";
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+?>
